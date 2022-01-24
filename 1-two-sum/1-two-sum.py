4e20221267b2268698e10159102413026fcfa6dd
Amulya Reddy <64305238+Amulya2823@users.noreply.github.com>
2022-01-24 22:46:45 +0530

    Time: 56 ms (92.70%), Space: 15.3 MB (42.44%) - LeetHub

---
+++ 1-two-sum/1-two-sum.py
@@ -0,0 +1,13 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        
+         
+        memo={}
+        
+        for i in range(len(nums)):
+            
+            if target-nums[i] in memo:
+                return [i,memo[target-nums[i]]]
+            
+            else:
+                memo[nums[i]]=i
