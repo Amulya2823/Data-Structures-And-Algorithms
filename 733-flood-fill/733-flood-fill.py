46975427c5e0a43a6e55cb1f06b9e67c4dc2c1f4
Amulya Reddy <64305238+Amulya2823@users.noreply.github.com>
2022-03-01 16:39:23 +0530

    Time: 107 ms (51.86%), Space: 14 MB (83.42%) - LeetHub

--- 733-flood-fill/733-flood-fill.py
+++ 733-flood-fill/733-flood-fill.py
@@ -2,29 +2,30 @@ class Solution:
     def floodFill(self, grid: List[List[int]], sr: int, sc: int, newColor: int) -> List[List[int]]:
         
         if grid[sr][sc] == newColor :
-            return grid
+            return grid 
         
-        self.floodAlgo(grid , sr , sc , grid[sr][sc] , len(grid) , len(grid[0]) , newColor) 
+        m = len(grid)
+        n = len(grid[0])
+        color = grid[sr][sc]
+        
+        
+        queue = collections.deque()
+        queue.append((sr , sc))
+        
+        while queue :
+            currentRow , currentCol = queue.popleft()
+            
+            
+            if currentRow < 0 or currentRow >= m or currentCol < 0 or currentCol >= n or grid[currentRow][currentCol]!= color :
+                continue
+                
+            grid [currentRow][currentCol] =  newColor  
+            queue.append((currentRow + 1 , currentCol))
+            queue.append((currentRow - 1 , currentCol))
+            queue.append((currentRow  , currentCol + 1))
+            queue.append((currentRow  , currentCol - 1))
+            
         return grid 
-    
-    def floodAlgo(self , grid , currentRow , currentCol , color , m , n , newColor):
-        
-        if currentRow < 0 or currentRow >= m or currentCol < 0 or currentCol >= n or grid[currentRow][currentCol]!= color :
-            return 
-        
-        grid [currentRow][currentCol] = newColor 
-        
-        #up
-        self.floodAlgo(grid , currentRow - 1 , currentCol , color , m , n , newColor)
-        
-        #down
-        self.floodAlgo(grid , currentRow + 1 , currentCol , color , m , n , newColor)
-        
-        #left
-        self.floodAlgo(grid , currentRow , currentCol - 1 , color , m , n , newColor)
-        
-        #right
-        self.floodAlgo(grid , currentRow , currentCol + 1 , color , m , n , newColor)
-        
-        return 
+            
+           
         
