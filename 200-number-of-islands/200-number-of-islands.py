a166baf6f42f958257b2e8bcfcf337fa2ee29d4d
Amulya Reddy <64305238+Amulya2823@users.noreply.github.com>
2022-03-02 16:02:25 +0530

    Time: 320 ms (85.60%), Space: 16.3 MB (89.18%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -1,35 +1,36 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         
+        answer = 0
         m = len(grid)
         n = len(grid[0])
-        answer = 0
         
-        
-        for currentRow in range (m):
-            for currentCol in range (n):
+        for currentRow in range(m):
+            for currentCol in range(n):
+                
                 if grid[currentRow][currentCol] == '1' :
                     answer += 1
                     self.numberIslands(grid , currentRow , currentCol , m , n)
                     
-        return answer
+        return answer 
     
-    def numberIslands(self , grid , currentRow , currentCol , m , n):
-        
-        if currentRow < 0 or currentRow >= m or currentCol < 0 or currentCol >= n or grid[currentRow][currentCol] == '0' :
-            return 
-        
-        grid [currentRow][currentCol] = '0'
-        
-        self.numberIslands(grid , currentRow + 1, currentCol , m , n)
-        self.numberIslands(grid , currentRow - 1, currentCol , m , n)
-        self.numberIslands(grid , currentRow , currentCol + 1, m , n)
-        self.numberIslands(grid , currentRow , currentCol - 1, m , n)
+    def numberIslands(self , grid , currentRow , currentCol , m ,n ):
+        queue = collections.deque()
+        queue.append((currentRow, currentCol))
         
         
-        return 
-    
-    
+        while queue :
+            currentRow,currentCol = queue.popleft()
+            
+            if currentRow < 0 or currentRow >= m or currentCol < 0 or currentCol >= n or grid[currentRow][currentCol] == '0' :
+                continue 
                 
-        
-        
+            grid[currentRow][currentCol] = '0'
+            queue.append((currentRow + 1, currentCol ))
+            queue.append((currentRow - 1, currentCol ))
+            queue.append((currentRow , currentCol + 1))
+            queue.append((currentRow , currentCol - 1))
+            
+        return 
+            
+            
