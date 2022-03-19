82be8d7a7ff475a0da46b3f4e93e8d884e649d97
Amulya Reddy <64305238+Amulya2823@users.noreply.github.com>
2022-03-19 21:39:54 +0530

    Time: 473 ms (42.50%), Space: 15 MB (53.25%) - LeetHub

---
+++ 494-target-sum/494-target-sum.py
@@ -0,0 +1,27 @@
+class Solution:
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        
+        return self.totalWays(nums , 0 , target , {})
+    
+    def totalWays(self , nums , currentIndex , target, memo) :
+        
+        if currentIndex == len(nums) and target == 0 :
+            return 1
+        
+        if currentIndex == len(nums) and target is not 0 :
+            return 0
+        
+        currentKey = (currentIndex , target)
+        
+        if currentKey in memo :
+            return memo[currentKey]
+        
+        
+        posSign = self.totalWays(nums , currentIndex+ 1 , target - nums[currentIndex] , memo)
+        negSign = self.totalWays(nums , currentIndex+ 1 , target + nums[currentIndex] , memo)
+        
+        memo[currentKey] = posSign + negSign
+        return memo[currentKey]
+        
+        
+        
