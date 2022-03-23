0bde1f49965a916f51a839d7e97ba00049abe234
Amulya Reddy <64305238+Amulya2823@users.noreply.github.com>
2022-03-23 22:14:57 +0530

    Time: 3973 ms (5.02%), Space: 156 MB (12.36%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
@@ -1,27 +1,38 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         
-        return self.buyandsell(prices,0,1,1,{})
+        return self.BuyandSell(prices , 0 ,1, 1 , {} )
     
-    def buyandsell(self,prices,currentDay,canBuy,transactionCount,memo):
+    def BuyandSell (self , prices , currentDay , canBuy , transCount , memo ) :
         
-        if currentDay >= len (prices) or transactionCount <= 0 :
+        if currentDay >= len(prices) or transCount <= 0 :
             return 0
         
-        currentkey = (currentDay,canBuy,transactionCount)
+        currentKey = (currentDay , canBuy ,  transCount )
         
-        if currentkey in memo:
-            return memo[currentkey]
+        if currentKey in memo :
+            return memo[currentKey]
+    
         
-        if canBuy == 1:
-            idle = self.buyandsell(prices,currentDay + 1 ,canBuy ,transactionCount,memo)
-            buy = self.buyandsell(prices,currentDay + 1 , 0 ,transactionCount,memo) - prices[currentDay]
-            memo[currentkey]  = max(idle,buy)
-            return memo[currentkey]
+        if canBuy == 1 :
             
-        else:
-            idle = self.buyandsell(prices,currentDay + 1 ,canBuy ,transactionCount,memo)
-            sell = self.buyandsell(prices,currentDay + 1 , 1 ,transactionCount - 1,memo) + prices[currentDay]
-            memo[currentkey] =  max(idle,sell)
-            return memo[currentkey]
+            idle = self.BuyandSell(prices , currentDay + 1 , canBuy , transCount , memo)
+            buy = self.BuyandSell(prices , currentDay + 1 , 0 , transCount , memo ) - prices[currentDay]
+            memo[currentKey] = max(idle , buy)
+            return memo[currentKey]
+        
+        else :
+            
+            idle = self.BuyandSell(prices , currentDay + 1 , canBuy , transCount , memo)
+            sell = self.BuyandSell(prices , currentDay + 1 , 1 , transCount - 1 , memo) + prices[currentDay]
+            memo[currentKey] = max(idle , sell)
+            return memo[currentKey]
+        
+        
+        
+        
+        
+        
+    
+    
         
