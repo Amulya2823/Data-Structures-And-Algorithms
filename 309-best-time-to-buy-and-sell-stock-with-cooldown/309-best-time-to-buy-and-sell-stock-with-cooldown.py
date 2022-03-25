2f3595007d4f8e5c83da67bec5c8c30a034eee53
Amulya Reddy <64305238+Amulya2823@users.noreply.github.com>
2022-03-25 11:00:27 +0530

    Time: 60 ms (57.78%), Space: 19.2 MB (16.13%) - LeetHub

---
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -0,0 +1,30 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        
+        return self.BuyandSell(prices , 0 , 1 , {})
+    
+    def BuyandSell(self , prices , currentDay , canBuy , memo ):
+        
+        if currentDay >= len(prices) :
+            return 0 
+        
+        currentKey = (currentDay , canBuy )
+        
+        if currentKey in memo:
+            return memo[currentKey]
+        
+        if canBuy == 1:
+            
+            idle = self.BuyandSell(prices , currentDay + 1 , canBuy , memo)
+            buy = self.BuyandSell(prices , currentDay + 1 , 0 , memo) - prices[currentDay]
+            memo[currentKey] = max(idle , buy)
+            return memo[currentKey]
+        
+        else:
+            
+            idle = self.BuyandSell(prices , currentDay +1 , canBuy , memo)
+            sell = self.BuyandSell(prices , currentDay + 2 , 1 , memo) + prices[currentDay]
+            memo[currentKey] = max(idle , sell)
+            return memo[currentKey]
+        
+        
