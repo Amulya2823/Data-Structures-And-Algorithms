9f05db97a9e950dc96e07dbdf6636f165d56967f
Amulya Reddy <64305238+Amulya2823@users.noreply.github.com>
2022-03-28 21:47:18 +0530

    Time: 203 ms (51.73%), Space: 15.9 MB (16.62%) - LeetHub

--- 931-minimum-falling-path-sum/931-minimum-falling-path-sum.py
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.py
@@ -3,32 +3,38 @@ class Solution:
         
         n = len(matrix)
         answer = 100000
-        memo ={}
+        memo = {}
         
-        for currentcolumn in range(0,n):
-            tempans = self.minimumsum(0,currentcolumn,matrix,n,memo)
-            answer = min(answer,tempans)
+        for currentCol in range(0 , n ):
+            tempAns = self.minimumFalling(matrix , 0 , currentCol , n ,memo )
+            answer = min (answer , tempAns)
             
         return answer 
-    def minimumsum(self,currentrow,currentcolumn,matrix,n,memo) :
+    
+    
+    def minimumFalling(self , matrix , currentRow , currentCol , n , memo) :
         
-        if currentrow >= n or currentrow < 0 or currentcolumn >= n or currentcolumn < 0 :
+        if currentRow < 0 or currentCol >= n or currentCol < 0 or currentCol >= n :
             return 100000
         
-        if currentrow == n-1 :
-            return matrix[currentrow][currentcolumn]
+        if currentRow == n-1 :
+            return matrix[currentRow][currentCol]
         
-        currentkey = (currentrow,currentcolumn)
+        currentKey = (currentRow , currentCol)
         
-        if currentkey in memo:
-            return memo[currentkey]
+        if currentKey in memo:
+            return memo[currentKey]
         
         
-        down = matrix[currentrow][currentcolumn] + self.minimumsum(currentrow +1 ,currentcolumn , matrix,n,memo)
-        diagleft = matrix[currentrow][currentcolumn] + self.minimumsum(currentrow +1 ,currentcolumn - 1 , matrix,n,memo)
-        diagright = matrix[currentrow][currentcolumn] + self.minimumsum(currentrow +1 ,currentcolumn + 1, matrix,n,memo)
+        down = matrix[currentRow][currentCol] + self.minimumFalling(matrix ,currentRow + 1 , currentCol , n , memo)
+        diagonalleft = matrix[currentRow][currentCol] + self.minimumFalling(matrix ,currentRow +1, currentCol -1 , n , memo)
+        diagonalright = matrix[currentRow][currentCol] + self.minimumFalling(matrix ,currentRow +1, currentCol + 1, n , memo)
         
-        memo[currentkey] = min(down,diagleft,diagright)
-        return memo[currentkey]
+        memo[currentKey] = min(down , diagonalleft,diagonalright)
+        return memo[currentKey]
+        
+        
+    
+
         
         
