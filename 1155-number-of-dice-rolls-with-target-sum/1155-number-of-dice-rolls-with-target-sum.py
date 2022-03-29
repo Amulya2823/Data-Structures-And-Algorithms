95a73f8663d8a4791947037b9d7328ccd68f8069
Amulya Reddy <64305238+Amulya2823@users.noreply.github.com>
2022-03-29 21:56:35 +0530

    Time: 1209 ms (13.03%), Space: 15.4 MB (49.78%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.py
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.py
@@ -1,37 +1,37 @@
 class Solution:
     def numRollsToTarget(self, n: int, k: int, target: int) -> int:
         
-        return self.diceRolls(n,k,target,{})
+        return self.diceRolls(n , k , target , {})
     
-    def diceRolls(self,dices,faces,target,memo)  :
+    def diceRolls(self , dices , faces , target , memo) :
         
         if dices == 0 and target == 0 :
             return 1
         
-        if dices == 0 and target is not 0:
+        if dices == 0 or target <= 0 :
             return 0
         
-        currentkey = (dices,target)
+        currentKey = ( dices , target)
         
-        if currentkey in memo:
-            return memo[currentkey]
+        if currentKey in memo :
+            return memo[currentKey]
         
         answer = 0
-        for i in range (1,faces + 1):
+        
+        
+        for i in range (1 , faces + 1) :
+            tempAns = self.diceRolls(dices -1 , faces , target - i , memo)
+            answer = (answer % 1000000007 + tempAns % 1000000007 ) % 1000000007 
             
-            tempanswer = self.diceRolls(dices - 1, faces , target - i,memo)
-            answer  = (answer % 1000000007 + tempanswer % 1000000007)%1000000007
             
-        memo[currentkey]  = answer
-        return memo[currentkey]
+        memo[currentKey] = answer 
+        return memo[currentKey]
             
         
             
         
-        
-        
-        
-        
-        
+            
+            
+            
         
         
