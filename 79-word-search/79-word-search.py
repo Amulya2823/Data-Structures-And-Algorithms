1254ba03c02d8a3b1f7fa0a7b29ac2d264fa193c
Amulya Reddy <64305238+Amulya2823@users.noreply.github.com>
2022-06-07 22:44:11 +0530

    Time: 7177 ms (47.53%), Space: 13.9 MB (50.67%) - LeetHub

---
+++ 79-word-search/79-word-search.py
@@ -0,0 +1,33 @@
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        
+        m = len(board)
+        n = len(board[0])
+        
+        for currentRow in range (m):
+            for currentCol in range (n):
+                
+                if board[currentRow][currentCol] == word[0] and self.findWord(board , currentRow , currentCol , 0 , word , m ,n):
+                    return True
+                
+        return False
+    
+    def findWord(self , board , currentRow , currentCol , currentIndex , word , m ,n):
+        
+        if currentIndex >= len(word):
+            return True
+        
+        if currentRow < 0 or currentRow >= m or currentCol < 0 or currentCol >= n or board[currentRow][currentCol] != word[currentIndex]:
+            return False
+        
+        temp = board[currentRow][currentCol]
+        board[currentRow][currentCol] = '.'
+        
+        found = self.findWord(board , currentRow - 1 , currentCol , currentIndex+1 , word, m , n) 
+        found = found or self.findWord(board , currentRow + 1 , currentCol , currentIndex+1, word,m , n) 
+        found = found or self.findWord(board , currentRow , currentCol-1 , currentIndex+1 , word , m , n) 
+        found = found or self.findWord(board , currentRow , currentCol+1 , currentIndex+1 , word,m , n)
+        
+        board[currentRow][currentCol] = temp
+        return found
+        
