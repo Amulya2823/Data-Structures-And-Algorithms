8406faf26a90377fdc6a6eda78630f207c269543
Amulya Reddy <64305238+Amulya2823@users.noreply.github.com>
2022-06-15 23:25:11 +0530

    Time: 442 ms (5.01%), Space: 14.2 MB (9.92%) - LeetHub

---
+++ 52-n-queens-ii/52-n-queens-ii.py
@@ -0,0 +1,94 @@
+class Solution:
+    def totalNQueens(self, n: int) -> int:
+        
+          
+        answer = []
+        board = [["." for i in range (n)]for j in range (n)]
+
+        for i in range (n):
+            for j in range (n):
+                board[i][j] = '.'
+                 
+                
+        self.nQueens(board , 0 , n , answer)
+        return len(answer)
+    
+    def nQueens(self , board , currentRow , n , answer):
+        
+        if currentRow == n:        #if you have reached end, u have an answer 
+            answer.append(self.constructAnswer(board,n))      
+            return 
+        
+        for currentCol in range (n):
+            
+            if(self.isValid(board , currentRow , currentCol , n)): #where can you place the queen
+                board[currentRow][currentCol] = "Q"
+                self.nQueens(board , currentRow + 1 , n , answer)
+                board[currentRow][currentCol] = "."            #undo the change
+                 
+        return 
+    
+    def constructAnswer(self , board , n):
+        
+        current = [] 
+        
+        for i in range (n):          #iterate over row
+            currentString = ""
+            for j in range (n):         #add as ..Q. 
+                currentString += board[i][j]
+                
+            current.append(currentString)   #add the 4 lines of board
+            
+        return current
+    
+    def isValid(self, board , currentRow , currentCol , n):
+        return self.isRowValid(board , currentRow , n) and self.isColValid(board , currentCol , n) and self.isDiagonalValid(board , currentRow , currentCol ,n)
+    
+    
+    def isRowValid(self , board ,currentRow , n):
+        
+        for currentCol in range (n):  #u have to check rows so iterate over columns
+            if board[currentRow][currentCol] == "Q":
+                return False
+            
+        return True
+    
+    def isColValid(self , board , currentCol,n):
+        
+        for currentRow in range(n):
+            if board[currentRow][currentCol] == "Q":
+                return False
+            
+        return True
+    
+    def isDiagonalValid(self , board , currentRow , currentCol , n):
+        
+        x = currentRow
+        y = currentCol
+        
+        while x >= 0 and y >= 0:
+            
+            if board[x][y] == "Q":
+                return False
+            
+            x -= 1
+            y -= 1
+            
+            
+        x = currentRow
+        y = currentCol
+        
+        while x >= 0 and y < n:
+            
+            if board[x][y] == "Q":
+                return False
+            
+            x -= 1
+            y += 1
+            
+        return True
+            
+      
+            
+        
+        
