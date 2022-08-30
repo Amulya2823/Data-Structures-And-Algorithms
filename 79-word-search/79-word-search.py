296ce2ed34cd11d07df2f4aa6ebafb73ef8aad38
Amulya Reddy <64305238+Amulya2823@users.noreply.github.com>
2022-08-30 06:09:42 +0530

    Time: 6098 ms (74.91%), Space: 13.9 MB (51.02%) - LeetHub

--- 79-word-search/79-word-search.py
+++ 79-word-search/79-word-search.py
@@ -4,30 +4,32 @@ class Solution:
         m = len(board)
         n = len(board[0])
         
-        for currentRow in range (m):
-            for currentCol in range (n):
+        for currentRow in range(m):
+            for currentCol in range(n):
                 
-                if board[currentRow][currentCol] == word[0] and self.findWord(board , currentRow , currentCol , 0 , word , m ,n):
-                    return True
+                if board[currentRow][currentCol] == word[0] and self.wordSearch(board , currentRow , currentCol , 0, word ,m , n):
+                    return True 
                 
         return False
     
-    def findWord(self , board , currentRow , currentCol , currentIndex , word , m ,n):
+    def wordSearch(self , board , currentRow , currentCol , currentIndex , word , m , n):
         
         if currentIndex >= len(word):
             return True
         
-        if currentRow < 0 or currentRow >= m or currentCol < 0 or currentCol >= n or board[currentRow][currentCol] != word[currentIndex]:
-            return False
+        if currentRow < 0 or currentRow >= m or currentCol < 0 or currentCol >= n or board[currentRow][currentCol] != word[currentIndex] :
+            return False 
         
         temp = board[currentRow][currentCol]
         board[currentRow][currentCol] = '.'
         
-        found = self.findWord(board , currentRow - 1 , currentCol , currentIndex+1 , word, m , n) 
-        found = found or self.findWord(board , currentRow + 1 , currentCol , currentIndex+1, word,m , n) 
-        found = found or self.findWord(board , currentRow , currentCol-1 , currentIndex+1 , word , m , n) 
-        found = found or self.findWord(board , currentRow , currentCol+1 , currentIndex+1 , word,m , n)
+        found = self.wordSearch(board , currentRow +1, currentCol , currentIndex+1 , word , m , n) or self.wordSearch(board , currentRow -1, currentCol , currentIndex+1 , word , m , n) or self.wordSearch(board , currentRow , currentCol+1 , currentIndex +1, word , m , n)or self.wordSearch(board , currentRow , currentCol-1, currentIndex +1, word , m , n)
         
-        board[currentRow][currentCol] = temp
+        board[currentRow][currentCol] = temp #after interating through all possible cond , change them to original
         return found
+        
+        
+                    
+        
+        
         
