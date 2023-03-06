5d269f64da84c5394fdc8e077af6cd51e06b7fe7
Amulya Reddy <64305238+Amulya2823@users.noreply.github.com>
2023-03-06 11:59:36 +0530

    Added solution - LeetHub

--- Pattern 5 - GFG/pattern-5.py
+++ Pattern 5 - GFG/pattern-5.py
@@ -3,7 +3,7 @@
 class Solution:
     def printTriangle(self, N):
         # Code here
-        for i in range(1, N+1):
+        for i in range(1,N+1):
             for j in range(N-i+1):
                 print("*" , end = " ")
             print()
